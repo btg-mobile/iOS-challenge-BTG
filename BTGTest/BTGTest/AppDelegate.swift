fcf26fb13c1a6ea21d52b041133b6a3af142e994
Mario de Castro <mario.castro@wappa.com.br>
2019-10-06 14:30:53 -0300

    created xcode project

---
+++ BTGTest/BTGTest/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  BTGTest
+//
+//  Created by Mario de Castro on 06/10/19.
+//  Copyright © 2019 Mario de Castro. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
