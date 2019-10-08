16a560135fba88fbeace57b29d6e0291a0eafc69
Ricardo Martins <rms.dev.ios@gmail.com>
2019-10-08 06:48:41 -0300

    Basic folder structure, custom lauch screen, resources, extensions, TabBar and gitignore

---
+++ RicardoMartinsFeatureDemo/RicardoMartinsFeatureDemo/MVVM/View/MoviesPopular/MoviesPopularVC.swift
@@ -0,0 +1,16 @@
+//
+//  MoviesPopularVC.swift
+//  RicardoMartinsFeatureDemo
+//
+//  Created by Ricardo Martins on 08/10/19.
+//  Copyright © 2019 https://ricardo.dev - Ricardo Martins. All rights reserved.
+//
+
+import UIKit
+
+class MoviesPopularVC: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
