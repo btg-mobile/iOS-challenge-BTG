5aabdaed613f832a2215508cc3412a76f910db96
LucasMW <great-lucas@hotmail.com>
2019-12-23 17:30:03 -0300

    Tests Base files and ignore

---
+++ BTG-Challenge/BTG-ChallengeTests/BTG_ChallengeTests.swift
@@ -0,0 +1,34 @@
+//
+//  BTG_ChallengeTests.swift
+//  BTG-ChallengeTests
+//
+//  Created by Lucas Menezes on 12/16/19.
+//  Copyright © 2019 Lucas Menezes. All rights reserved.
+//
+
+import XCTest
+@testable import BTG_Challenge
+
+class BTG_ChallengeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
