e36eaf79605b8239a51bfed334b4f5e2a21d9561
LucasMW <great-lucas@hotmail.com>
2019-12-23 17:25:10 -0300

    Changed Loading Screen

--- BTG-Challenge/BTG-Challenge/Views/MovieViewController.swift
+++ BTG-Challenge/BTG-Challenge/Views/MovieViewController.swift
@@ -40,20 +40,6 @@ class MovieViewController: UIViewController, NVActivityIndicatorViewable{
         // Do any additional setup after loading the view.
     }
 
-    
-    
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 extension MovieViewController: UITableViewDelegate, UITableViewDataSource {
     
