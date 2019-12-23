fd45c95d8dd00a4ba2cc69a1e52e614d80d396f2
LucasMW <great-lucas@hotmail.com>
2019-12-23 16:27:27 -0300

    Basic Search

--- BTG-Challenge/BTG-Challenge/Views/MovieViewController.swift
+++ BTG-Challenge/BTG-Challenge/Views/MovieViewController.swift
@@ -10,13 +10,17 @@ import UIKit
 import NVActivityIndicatorView
 class MovieViewController: UIViewController, NVActivityIndicatorViewable{
     
+    @IBOutlet weak var searchBar: UISearchBar!
     var array = [Results]()
+    var displayArray = [Results]()
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Load Movie")
         tableView.delegate = self
         tableView.dataSource = self
+        searchBar.delegate = self
+        
         let api = API()
         self.startAnimating()
         if allGenres.isEmpty {
@@ -29,11 +33,13 @@ class MovieViewController: UIViewController, NVActivityIndicatorViewable{
         }
         api.requestPopular { (results) in
             self.array = results ?? []
+            self.displayArray = self.array
             self.tableView.reloadData()
             self.stopAnimating()
         }
         // Do any additional setup after loading the view.
     }
+
     
     
 
@@ -52,14 +58,14 @@ class MovieViewController: UIViewController, NVActivityIndicatorViewable{
 extension MovieViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return array.count
+        return displayArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieTableViewCell") as? MovieTableViewCell ?? MovieTableViewCell()
-        cell.title.text = array[indexPath.row].title
-        cell.yearLable.text = array[indexPath.row].release_date
-        cell.picture.imageFromURL(urlString: API().getPictureString(path: array[indexPath.row].poster_path ?? ""))
+        cell.title.text = displayArray[indexPath.row].title
+        cell.yearLable.text = displayArray[indexPath.row].release_date
+        cell.picture.imageFromURL(urlString: API().getPictureString(path: displayArray[indexPath.row].poster_path ?? ""))
         return cell
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -70,7 +76,35 @@ extension MovieViewController: UITableViewDelegate, UITableViewDataSource {
             guard let vc = segue.destination as? MovieDetailViewController else {
                 return
             }
-            vc.movieDetail = array[tableView.indexPathForSelectedRow?.row ?? 0]
+            vc.movieDetail = displayArray[tableView.indexPathForSelectedRow?.row ?? 0]
         }
     }
+}
+extension MovieViewController : UISearchBarDelegate, UISearchControllerDelegate {
+
+     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+           print("should end keyboard")
+           self.view.endEditing(true)
+       }
+       func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+           print("should end keyboard")
+           self.view.endEditing(true)
+       }
+       func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+           print("search \(searchText)")
+           
+            let filtered = array.filter({ (r) -> Bool in
+                return r.title!.lowercased().contains(searchText.lowercased())
+            })
+               
+        print(filtered.map({$0.title}))
+               if filtered.isEmpty == false {
+                   self.displayArray = filtered
+               } else {
+                   self.displayArray = array
+                
+               }
+        self.tableView.reloadData()
+           }
+
 }
